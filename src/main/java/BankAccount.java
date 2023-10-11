d36968b5cc08cd424844133c12dce3efe51604f6
Albert Hughes <albieh@me.com>
2023-10-11 16:00:56 +0100

    Added getters and setters to BankAccount.java

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,56 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+
+    private String firstName = "";
+    private String lastName = "";
+    private LocalDate dateOfBirth;
+    private int accountNumber = 0;
+    private int balance = 0;
+
+
+    public BankAccount(){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+
+    }
+
+//    Below are the getters.
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public LocalDate getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    public int getBalance(){
+        return this.balance;
+    }
+
+//    Below will be the setters
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber = accountNumber;
+    }
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+
 }
