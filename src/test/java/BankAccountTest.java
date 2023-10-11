34d144d2dabb3f88e489bba66cfddbd530697eaa
Albert Hughes <albieh@me.com>
2023-10-11 15:25:27 +0100

    Initial comments for the unit tests outlining thought process

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,43 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat; // Will be used at one point
+
+
 public class BankAccountTest {
+
+    @Test
+    void canGetBankAccountDetails(){
+//      This unit test should test the getter functions for the BankAccount properties.
+//      There should be a getter for each property so: getFirstName, getLastName, getDOB, GetAccountNumber,
+//      and getAccountBalance
+
+    }
+
+    @Test
+    void canSetBankAccountDetails(){
+//        This unit test should test all setter functions for the BankAccount Properties.
+//        There should be a setter for each property: so setFirstName, setLastName, setDOB, setAccountNumber
+//        and setAccountBalance
+
+    }
+
+    @Test
+    void canDeposit(){
+//        This will test the deposit method on the bank account object.
+    }
+
+    @Test
+    void canWithdraw(){
+//        This will test the deposit method on the bank account object.
+    }
+
+    @Test
+    void canPayInterest(){
+//        This will test the payinterest method on the bank account object.
+    }
+
+
+
+
+
+
 }
